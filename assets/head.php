a077101908790d60ccfc9e1c94604f3f1edb1c32
aroceu <dev@aroceu.cloud>
2025-05-12 23:33:07 -0400

    Added head/preview

--- assets/head.php
+++ assets/head.php
@@ -2,12 +2,16 @@
 <html>
 <head>
     <title>ARROW.iKA</title>
-    <meta name="robots" content="noai, noimageai">    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+   	<meta name="robots" content="noai, noimageai">
+	<meta charset="UTF-8">
+    <meta name="description" content="i wish humans were real."/>
+	<meta property="og:title" content="aroceu (splatoon)" />
+	<meta property="og:description" content="i wish humans were real." />
+	<meta property="og:image" content="https://aroceu.com/splatoon/assets/preview.png" />
+	<meta property="og:url" content="https://aroceu.com/splatoon" />
+	<meta name="viewport" content="initial-scale=1.0, maximum-scale=5, width=device-width">
     <link rel="shortcut icon" type="image/x-icon" href="assets/fav.png" />
     <link rel="stylesheet" type="text/css" href="assets/style.css">
-    <meta name="description" content="i wish humans were real."/>
 </head>
 
 
